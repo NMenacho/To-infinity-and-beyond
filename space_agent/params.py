59fc9f62135010995177d2070ac73934df8ec2b8
Sarah Violette Akamiezan <sarah.akamiezan@gmail.com>
2024-06-06 15:19:16 +0200

    models trained

--- space_agent/params.py
+++ space_agent/params.py
@@ -2,8 +2,9 @@ import os
 
 path = os.getcwd()
 
-DIR_PATH = os.path.abspath(os.path.join(path))
+DIR_PATH = os.path.abspath(os.path.dirname(__file__))
 DATA_PATH = os.path.abspath(os.path.join(path, 'data'))
 DATA_IMAGES_CROP = os.path.abspath(os.path.join(path, 'data','images_cropped'))
 LOGS_IMAGES = os.path.abspath(os.path.join(path, 'logs'))
-IMG_FOLDER_SAMPLE = os.path.abspath(os.path.join(path, 'data','images_cropped_sample')
+IMG_FOLDER_SAMPLE = os.path.abspath(os.path.join(path, 'data','images_cropped_sample'))
+print('Directory Name: ', os.path.dirname(__file__))
