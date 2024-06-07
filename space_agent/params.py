26f97589f3709c2a6b9354ac53bc97ff166c11b0
Sebastien VINCHON <goboco007@gmail.com>
2024-06-07 12:38:49 +0200

    enriched params.py

--- space_agent/params.py
+++ space_agent/params.py
@@ -1,2 +1,8 @@
 
 # TODO: initialize all needed env variables here
+
+PROJECT_FOLDER='/Users/svinchon/code/NMenacho/To-infinity-and-beyond'
+AUGMENTED_IMAGES_FOLDER=f'{PROJECT_FOLDER}/data/images_cropped_from_augmentation'
+DOWNLOADED_IMAGES_FOLDER=f'{PROJECT_FOLDER}/data/images_cropped_sample'
+MODELS_FOLDER=f'{PROJECT_FOLDER}/models'
+UPLOADED_IMAGES_FOLDER=f'{PROJECT_FOLDER}/uploaded_images'
